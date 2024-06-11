ca357738e8764a0bc10103110cdfac9f1421c52e
Tamana <tamana.amraji@gmail.com>
2024-06-11 13:24:22 +0330

    sound buzzer, 7seg display, 7seg refreshing done

--- CodeVision/Mid.c
+++ CodeVision/Mid.c
@@ -2,6 +2,11 @@
 #include <delay.h>
 // Declare your global variables here
 int LCD_ref=0xEF, i=0, digit[2];
+    int row = -1, col =-1, input = -1;
+#define col0    PINA.4
+#define col1    PINA.5
+#define col2    PINA.6
+#define col3    PINA.7  
 
     const int segments[] =
      {
@@ -16,46 +21,50 @@ int LCD_ref=0xEF, i=0, digit[2];
         0x78, // 7
         0x00, // 8
         0x10 // 9
-    };
+    };  
+    
+    char row_ref[]= {
+            0b00000001,
+            0b00000010,
+            0b00000100,
+            0b00001000
+            };  
 
 // Function prototypes
 void display_on_seven_segment(int number);
+void sound_buzzer();
 
 // Timer 0 output compare interrupt service routine
 interrupt [TIM0_COMP] void timer0_comp_isr(void)
 {
-// Place your code here
-    PORTA = ~LCD_ref;  
-    PORTC = ~(segments[digit[i]]); 
+ Place your code here
+    PORTB = LCD_ref;   
     LCD_ref = LCD_ref<<1; 
     i++;
     if (i == 4) 
     {
         i = 0;
         LCD_ref = 0xEF;
-    } 
+    }   
+//    
+ 
 }
 
 void main(void)
 {
 // Declare your local variables here
-
-// Input/Output Ports initialization
-// Port A initialization
+int key;  int minutes = 0,seconds = 0;
 
     // Initialize ports and pins
     // Configure SEVENSEG_PORT, KEYPAD_PORT, BUZZER_PORT, and LED_PORT as required 
     
-    DDRB = 0x10000000;
-    
     // Set ROWS as outputs and COLS as inputs
     //DDRA = 0x0F; // Assuming keypad is connected to PORTA pins 0-3 as ROWS
-    DDRB = 0x0F; // Assuming keypad is connected to PORTB pins 0-3 as COLS
-    PORTA = 0x0F; // Activate internal pull-ups on PORTA pins 0-3 as ROWS 
-    DDRA=(1<<DDD7) | (1<<DDD6) | (1<<DDD5) | (1<<DDD4) | (0<<DDD3) | (0<<DDD2) | (0<<DDD1) | (0<<DDD0);   
-    PORTB = 0xF0; // Activate internal pull-ups on PORTB pins 0-3 as COLS    
+    DDRB = 0xFF; // Assuming keypad is connected to PORTB pins 0-3 as COLS
+    PORTB = 0x00; // Activate internal pull-ups on PORTB pins 0-3 as COLS    
+    PORTA = 0x00; // Activate internal pull-ups on PORTA pins 0-3 as ROWS 
+    DDRA=(0<<DDA7) | (0<<DDA6) | (0<<DDA5) | (0<<DDA4) | (1<<DDA3) | (1<<DDA2) | (1<<DDA1) | (1<<DDA0);;       
     DDRC = 0xFF; // Assuming seven segment display is connected to PORTC  
-    DDRB=(1<<DDB7); // Assuming buzzer is connected to pin PB0
     DDRD = 0xFF; // Assuming LEDs are connected to PORTD   
 
 
@@ -93,61 +102,26 @@ OCR1AL=0x11;
 OCR1BH=0x00;
 OCR1BL=0x00;
 
-// Timer/Counter 2 initialization
-// Clock source: System Clock
-// Clock value: Timer2 Stopped
-// Mode: Normal top=0xFF
-// OC2 output: Disconnected
-ASSR=0<<AS2;
-TCCR2=(0<<PWM2) | (0<<COM21) | (0<<COM20) | (0<<CTC2) | (0<<CS22) | (0<<CS21) | (0<<CS20);
-TCNT2=0x00;
-OCR2=0x00;
 
 // Timer(s)/Counter(s) Interrupt(s) initialization
 TIMSK=(0<<OCIE2) | (0<<TOIE2) | (0<<TICIE1) | (1<<OCIE1A) | (1<<OCIE1B) | (0<<TOIE1) | (1<<OCIE0) | (0<<TOIE0);
 
-// External Interrupt(s) initialization
-// INT0: Off
-// INT1: Off
-// INT2: Off
-MCUCR=(0<<ISC11) | (0<<ISC10) | (0<<ISC01) | (0<<ISC00);
-MCUCSR=(0<<ISC2);
 
-// USART initialization
-// USART disabled
-UCSRB=(0<<RXCIE) | (0<<TXCIE) | (0<<UDRIE) | (0<<RXEN) | (0<<TXEN) | (0<<UCSZ2) | (0<<RXB8) | (0<<TXB8);
-
-// Analog Comparator initialization
-// Analog Comparator: Off
-// The Analog Comparator's positive input is
-// connected to the AIN0 pin
-// The Analog Comparator's negative input is
-// connected to the AIN1 pin
-ACSR=(1<<ACD) | (0<<ACBG) | (0<<ACO) | (0<<ACI) | (0<<ACIE) | (0<<ACIC) | (0<<ACIS1) | (0<<ACIS0);
-SFIOR=(0<<ACME);
-
-// ADC initialization
-// ADC disabled
-ADCSRA=(0<<ADEN) | (0<<ADSC) | (0<<ADATE) | (0<<ADIF) | (0<<ADIE) | (0<<ADPS2) | (0<<ADPS1) | (0<<ADPS0);
-
-// SPI initialization
-// SPI disabled
-SPCR=(0<<SPIE) | (0<<SPE) | (0<<DORD) | (0<<MSTR) | (0<<CPOL) | (0<<CPHA) | (0<<SPR1) | (0<<SPR0);
-
-// TWI initialization
-// TWI disabled
-TWCR=(0<<TWEA) | (0<<TWSTA) | (0<<TWSTO) | (0<<TWEN) | (0<<TWIE);
-
+ // Read minutes input from keypad      
 // Global enable interrupts
 #asm("sei")
-
 while (1)
       {
-      // Place your code here
-        display_on_seven_segment(25);
-      //PORTC = ~(0x10);
+      // Place your code here 
+       
+//      key = read_keypad();     
+      //display_on_seven_segment(key);
+
       }
 }
+
+
+
 
 void display_on_seven_segment(int number) {
     
@@ -166,9 +140,13 @@ void display_on_seven_segment(int number) {
         0x78, // 7
         0x00, // 8
         0x10 // 9
-    };
+    };      
+    
+
 
     // Extract digits from the number
      digit[1] = number / 10;
      digit[2] = number % 10; 
+     
+     //PORTC = ~(segments[digit[i]]);
 }
